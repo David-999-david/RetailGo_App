3cf648d740d5048efd545d5ed2fe1b66ea94cf80
David-999-david <dontgun54@gmail.com>
2025-02-21 10:45:30 +0630

    onboard ui

---
+++ lib/presentation/onboard/data/onboard_data.dart
@@ -0,0 +1,17 @@
+const List<Map<String, String>> OnBoardData = [
+  {
+    'image': 'assets/onboardImage/doggy1.webp',
+    'title': 'ARE YOU SURE!',
+    'description': 'ON HERE,YOU CAN GET A GIRLFRIEND...',
+  },
+  {
+    'image': 'assets/onboardImage/doggy3.jpg',
+    'title': 'ARE YOU SURE!',
+    'description': 'ON HERE,YOU CAN GET A GIRLFRIEND...',
+  },
+  {
+    'image': 'assets/onboardImage/doggy4.webp',
+    'title': 'ARE YOU SURE!',
+    'description': 'ON HERE,YOU CAN GET A GIRLFRIEND...',
+  }
+];
